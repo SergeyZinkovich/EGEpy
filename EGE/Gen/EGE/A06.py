2e64ef6143201a6276a45b37b54dbf43ef37f6b4
Anton Podchuvalov <podchuvalovvl@gmail.com>
2021-12-19 22:12:27 +1000

    Add base for A6

---
+++ EGE/Gen/EGE/A06.py
@@ -0,0 +1,45 @@
+from ...GenBase import SingleChoice
+
+class CountBySign(SingleChoice):
+    def generate(self):
+        return super().generate()
+
+class FindMinMax(SingleChoice):
+    def generate(self):
+        return super().generate()
+
+class CountOddEven(SingleChoice):
+    def generate(self):
+        return super().generate()
+
+class AlgMinMax(SingleChoice):
+    def generate(self):
+        return super().generate()
+
+class AlgAgv(SingleChoice):
+    def generate(self):
+        return super().generate()
+
+class BusStation(SingleChoice):
+    def generate(self):
+        return super().generate()
+
+    def __randomRoutes(self):
+        pass
+
+    def __genScheduleText(self):
+        pass
+
+    def __findAllRoutes(self):
+        pass
+
+class CRCMessage(SingleChoice):
+    def generate(self):
+        return super().generate()
+
+class InfSize(SingleChoice):
+    def generate(self):
+        return super().generate()
+
+def __stime():
+    pass
